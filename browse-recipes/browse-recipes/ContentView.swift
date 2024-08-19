e9b5097880b46226bca6e6863331087b8569aefc
Kanjiz1 <kanjis007@gmail.com>
2024-08-19 11:02:53 -0500

    Adding XCode Project.

---
+++ browse-recipes/browse-recipes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  browse-recipes
+//
+//  Created by Oforkanji Odekpe on 8/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ browse-recipes/browse-recipes/browse_recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  browse_recipesApp.swift
+//  browse-recipes
+//
+//  Created by Oforkanji Odekpe on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct browse_recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
